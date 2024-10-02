61775b2e69c07e82b6e1ced5b23875021da33439
mcgear <michael.gearhardt@gmail.com>
2024-10-01 23:27:03 -0600

    ref updates and missing export

--- src/atoms/.exports.ts
+++ src/atoms/.exports.ts
@@ -4,6 +4,9 @@ export {
   type ActionButtonProps,
   type ActionProps,
   ActionStyleTypes,
+  type BrowserNavBlockProps,
+  Icon,
+  type IconProps,
 } from '../src.deps.ts';
 
 export * from './forms/.exports.ts';
